8349332cd07eb70e16d2a32522e3b97fe36a310d
Brandon Hays <brandon.hays@gmail.com>
2014-04-30 00:18:55 -0500

    improve API for sensor setup

--- lib/brick_pi/sensor.rb
+++ lib/brick_pi/sensor.rb
@@ -1,9 +1,19 @@
 module BrickPi
   class Sensor
+    SENSOR_TYPES = {
+      touch: Native::TYPE_SENSOR_TOUCH,
+      ultrasonic: Native::TYPE_SENSOR_ULTRASONIC_CONT,
+      color: Native::TYPE_SENSOR_COLOR_FULL,
+      color_red: Native::TYPE_SENSOR_COLOR_RED,
+      color_green: Native::TYPE_SENSOR_COLOR_GREEN,
+      color_blue: Native::TYPE_SENSOR_COLOR_BLUE,
+      color_none: Native::TYPE_SENSOR_COLOR_NONE
+    }
+
     def initialize(port, sensor_type)
       @port = port
       @sensor_type = sensor_type
-      Native::SensorType[@port] = @sensor_type
+      Native::SensorType[@port] = SENSOR_TYPES[@sensor_type]
       Native::BrickPiSetupSensors()
     end
 
